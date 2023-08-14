d6a41d7eeca915f4fb1d6ac3a7d24b40f125478c
badrxd <badrxdxd@gmail.com>
2023-08-14 02:04:52 +0100

    ok

---
+++ tests/test_console.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+console test
+"""
+import json
+import unittest
+from models.base_model import BaseModel
+
+
+class ConsoleTestCase(unittest.TestCase):
+    """ Unittest for console class """
+    def test_do_all(self):
+        """ test Class attributes """
+        pass
